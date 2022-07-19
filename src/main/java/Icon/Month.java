6976daa8fefecfeafa32d5419627f142adeed358
Bohdan <l0mobot50@gmail.com>
2022-07-19 17:48:50 +0300

    chapter 15

---
+++ src/main/java/Icon/Month.java
@@ -0,0 +1,27 @@
+package Icon;
+
+//public enum Month {
+//    DECEMBER("December"),
+//    NOVEMBER("November"),
+//    January,
+//    February,
+//    March,
+//    April,
+//    May,
+//    June,
+//    July,
+//    August,
+//    September,
+//    October,
+//    November;
+//
+//    String name;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    Month(String name) {
+//        this.name = name;
+//    }
+//}
